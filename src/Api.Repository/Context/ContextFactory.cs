844e353703d659b562cb2a5ba2453f23f2bc0f87
Moises R. Souza <loveloungeoficial@gmail.com>
2020-09-20 18:06:07 -0300

    Api.Repository Criar ContextFactory

---
+++ src/Api.Repository/Context/ContextFactory.cs
@@ -0,0 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Api.Repository.Context
+{
+    public class ContextFactory : IDesignTimeDbContextFactory<MyContext>
+    {
+        public MyContext CreateDbContext(string[] args)
+        {
+            //Usado para Criar as Migrações
+            //var connectionString = "Server=localhost;Port=3306;Database=dbAPI;Uid=root;Pwd=mudar@123";
+            //TODO: mudar senha
+            var connectionString = "Server=.\\SQLEXPRESS2017;Database=dbAPI;User Id=sa;Password=mudar@123";
+            var optionsBuilder = new DbContextOptionsBuilder<MyContext>();
+            //optionsBuilder.UseMySql (connectionString);
+            optionsBuilder.UseSqlServer(connectionString);
+            return new MyContext(optionsBuilder.Options);
+        }
+    }
+}
