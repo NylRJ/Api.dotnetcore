7b1e64ddb06a0a79c3dedc4dbf926defae0688d9
Moises R. Souza <loveloungeoficial@gmail.com>
2020-09-20 17:10:54 -0300

    ValueObject

--- src/Api.Domain/Entities/UserEntity/ValueObject/Email.cs
+++ src/Api.Domain/Entities/UserEntity/ValueObject/Email.cs
@@ -1,7 +1,61 @@
-namespace Api.Domain.Entities.UserEntity.aggregates
+using System;
+using System.Text.RegularExpressions;
+
+namespace Api.Domain.Entities.UserEntity.ValueObject
 {
-    public class Email
+
+    public class Email : IEquatable<Email>, IEquatable<string>
     {
-        
+        public string Value { get; }
+
+        public Email(string value)
+        {
+            if (!value.Contains("@")) throw new Exception("Email is invalid");
+
+            Value = value;
+        }
+
+        #region Conversion
+
+        public static implicit operator string(Email value)
+        {
+            return value.Value;
+        }
+
+        public static implicit operator Email(string value)
+        {
+            return new Email(value);
+        }
+
+        #endregion
+
+        #region Equality
+
+        public override bool Equals(object obj)
+        {
+            var other = obj as Email;
+
+            return other != null ? Equals(other) : Equals(obj as string);
+        }
+
+        public bool Equals(Email other) => other != null && Value == other.Value;
+
+        public bool Equals(string other) => Value == other;
+
+        public static bool operator ==(Email a, Email b)
+        {
+            if (ReferenceEquals(a, b)) return true;
+            if (((object)a == null) || ((object)b == null)) return false;
+
+            return a.Value == b.Value;
+        }
+
+        public static bool operator !=(Email a, Email b) => !(a == b);
+
+        #endregion
+
+        public override int GetHashCode() => Value.GetHashCode();
+
+        public override string ToString() => Value;
     }
 }
