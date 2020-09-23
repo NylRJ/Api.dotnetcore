93ce7ebce94f42583276ebc800d1f02c51fca0a9
Moises R. Souza <loveloungeoficial@gmail.com>
2020-09-23 02:21:39 -0300

    Api.Application - Implementação do  Swagger

--- src/Api.Application/Startup.cs
+++ src/Api.Application/Startup.cs
@@ -1,17 +1,11 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Api.CrossCutting.DependencyInjection;
-using Api.Repository.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace application
 {
@@ -30,8 +24,29 @@ namespace application
 
             ConfigureService.ConfigureDependenciesService(services);
             ConfigureRepository.ConfigureDependenciesRepository(services);
-
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Assessment Melhores Práticas AspNetCore 3.1",
+                    Description = "Arquitetura DDD",
+                    TermsOfService = new Uri("https://github.com/NylRJ/Api.dotnetcore"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Moisés Rodrigues",
+                        Email = "moises.souza@al.infnet.edu.br",
+                        Url = new Uri("https://www.linkedin.com/in/moises-rodrigues-de-souza-4936591b7/")
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "Termo de Licença de Uso",
+                        Url = new Uri("https://www.linkedin.com/in/moises-rodrigues-de-souza-4936591b7/")
+                    }
+                });
+            });
+            //services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,6 +56,13 @@ namespace application
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Assessment Engenharia da Computação => Melhores Prática AspNetCore 3.1");
+                c.RoutePrefix = string.Empty;
+            });
 
             app.UseRouting();
 
