0667d038c7d2fd7eb909dcdcd8a980fc52b00286
Moises R. Souza <loveloungeoficial@gmail.com>
2020-09-24 16:49:28 -0300

    Implement Login Controller Service, Api.CrossCutting - Injeção de Dependências

--- src/Api.CrossCutting/DependencyInjection/ConfigureRepository.cs
+++ src/Api.CrossCutting/DependencyInjection/ConfigureRepository.cs
@@ -1,5 +1,7 @@
 using Api.Domain.interfaces;
+using Api.Domain.Repository;
 using Api.Repository.Context;
+using Api.Repository.Implementations;
 using Api.Repository.Repository;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
@@ -12,6 +14,10 @@ namespace Api.CrossCutting.DependencyInjection
         public static void ConfigureDependenciesRepository(IServiceCollection serviceCollection)
         {
             serviceCollection.AddScoped(typeof(IRepository<>), typeof(BaseRepository<>));
+            serviceCollection.AddScoped<IUserRepository, UserImplementation>();
+
+
+
             serviceCollection.AddDbContext<MyContext>(
                 options => options.UseMySql("Server=localhost;Port=3306;database=dbAPI;Uid=root;Pwd=J1r@y@;")
             );
--- src/Api.Domain/interfaces/Services/User/ILoginService.cs
+++ src/Api.Domain/interfaces/Services/User/ILoginService.cs
@@ -1,7 +1,10 @@
+using System.Threading.Tasks;
+using Api.Domain.Entities.UserEntity;
+
 namespace Api.Domain.interfaces.Services.User
 {
     public interface ILoginService
     {
-
+        Task<object> FindByLogin(UserEntity user);
     }
 }
