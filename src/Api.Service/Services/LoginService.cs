c719f7e2b2dd7e07066fc2cdd825a3ffe6fdde96
Moises R. Souza <loveloungeoficial@gmail.com>
2020-09-24 02:40:15 -0300

    Api.Service - LoginService

---
+++ src/Api.Service/Services/LoginService.cs
@@ -0,0 +1,31 @@
+using System.Threading.Tasks;
+using Api.Domain.Entities.UserEntity;
+using Api.Domain.interfaces.Services.User;
+using Api.Domain.Repository;
+
+namespace Api.Service.Services
+{
+    public class LoginService : ILoginService
+    {
+        private IUserRepository _repository;
+        public LoginService(IUserRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<object> FindByLogin(UserEntity user)
+        {
+
+            if (user != null && !string.IsNullOrWhiteSpace(user.Email))
+            {
+                return await _repository.FindByLogin(user.Email);
+
+            }
+            else
+            {
+                return null;
+            }
+
+        }
+    }
+}
