5050f8f8fa2a39008cb9b37d5b74ef481dcebf3f
Moises R. Souza <loveloungeoficial@gmail.com>
2020-09-25 00:31:32 -0300

    Implementar LoginDto no lugar de UserEntity nas operações referente ao Login

--- src/Api.Domain/interfaces/Services/User/ILoginService.cs
+++ src/Api.Domain/interfaces/Services/User/ILoginService.cs
@@ -1,10 +1,10 @@
 using System.Threading.Tasks;
-using Api.Domain.Entities.UserEntity;
+using Api.Domain.Dtos;
 
 namespace Api.Domain.interfaces.Services.User
 {
     public interface ILoginService
     {
-        Task<object> FindByLogin(UserEntity user);
+        Task<object> FindByLogin(LoginDto user);
     }
 }
